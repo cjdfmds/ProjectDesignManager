2eed76d26c1799ce878a97f20fdd97ead19c1566
Zerian <48261394+cjdfmds@users.noreply.github.com>
2024-10-23 23:22:50 +1300

    please work

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,11 @@
+  const awsConfig = {
+    Auth: {
+        Cognito: {
+            region: 'ap-southeast-2',
+            userPoolId: "ap-southeast-2_sU6dOKhL2",
+            userPoolClientId: "44005cgeeckdt62jpfv7nmpbcf"           
+        }
+    }
+};
+
+export default awsConfig;
